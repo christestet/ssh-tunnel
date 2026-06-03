c85fd3f823dc5e2cce706c1d5905b521d0d5dc41
christestet <christestautomation@gmail.com>
2026-06-03 22:32:27 +0200

    fix: remove unsupported Swift language mode setting

--- Package.swift
+++ Package.swift
@@ -12,20 +12,17 @@ let package = Package(
     targets: [
         .target(
             name: "SSHTunnelKit",
-            path: "Sources/SSHTunnelKit",
-            swiftSettings: [.swiftLanguageMode(.v6)]
+            path: "Sources/SSHTunnelKit"
         ),
         .executableTarget(
             name: "SSHTunnel",
             dependencies: ["SSHTunnelKit"],
-            path: "Sources/SSHTunnel",
-            swiftSettings: [.swiftLanguageMode(.v6)]
+            path: "Sources/SSHTunnel"
         ),
         .testTarget(
             name: "SSHTunnelTests",
             dependencies: ["SSHTunnelKit"],
-            path: "Tests/SSHTunnelTests",
-            swiftSettings: [.swiftLanguageMode(.v6)]
+            path: "Tests/SSHTunnelTests"
         )
     ]
 )
